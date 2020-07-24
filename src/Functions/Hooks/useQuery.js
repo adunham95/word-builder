c16bb7e4c1f947298b5feadd86e72bd1b4ce48fa
Adrian <adunham95@gmail.cooom>
2020-07-24 15:29:33 -0400

    Removed unused console

--- src/Functions/Hooks/useQuery.js
+++ src/Functions/Hooks/useQuery.js
@@ -1,9 +1,6 @@
 export function useQuery(){
     const urlParams = new URLSearchParams(window.location.search);
     let urlData = {}
-
-    console.log(urlParams)
-
     urlParams.forEach(function(value, key) {
         urlData[key] = value;
       });
