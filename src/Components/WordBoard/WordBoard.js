592ea14a3b71d58308c40cb5747e2b26b2f89847
Adrian <adunham95@gmail.cooom>
2020-07-24 17:36:58 -0400

    Added filter for max level and imports

--- src/Components/WordBoard/WordBoard.js
+++ src/Components/WordBoard/WordBoard.js
@@ -5,6 +5,8 @@ import Letter from '../Letter/Letter';
 import { LetterList } from '../LetterList/LetterList';
 import "./WordBoard.scss"
 import { Trashcan } from '../TrashCan/TrashCan';
+import letterData from "../../Data/letters.json"
+import { useQuery } from '../../Functions/Hooks/useQuery';
 
 function renderLetters(i, letter){
   return(
@@ -16,12 +18,22 @@ function renderLetters(i, letter){
 
 
 export const WordBoard = () => {
-    
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+  const query = useQuery();
+
+
+  let maxLevel = 1.1;
+  if(typeof query.section !== "undefined" && query.section !== ""){
+    maxLevel = parseFloat(query.section);
+  }
+
+  console.log("maxLevel", maxLevel)
+
+  const alphabet = letterData.filter(d => d.level < (maxLevel + 0.1))
   const renderedAlphabet = []
 
   for (let i = 0; i < alphabet.length; i++) {
-    renderedAlphabet.push(renderLetters(i, alphabet[i]))
+    renderedAlphabet.push(renderLetters(i, alphabet[i].letter))
 }
 
   return (
